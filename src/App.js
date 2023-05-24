5ddc1166d10526f7cc8eed6f6cd6a9967c0955ee
Courtney Frey <66076696+speudusa@users.noreply.github.com>
2023-05-24 13:24:50 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Buttons from "./components/Buttons";
+import Table from "./components/Table";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Buttons />
+      <Table />
+    </div>
+  );
+}
